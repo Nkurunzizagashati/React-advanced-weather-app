cee8c14043a1a21feacc8f1927887c0cdec35fe3
Fabrice <f.nkurunziz@alustudent.com>
2024-09-14 01:34:30 +0200

    weather forecasting

--- src/components/Map.tsx
+++ src/components/Map.tsx
@@ -63,40 +63,52 @@ const MapComponent: React.FC<MapComponentProps> = ({
 	// 			</div>
 	// 			<span className="sr-only">Loading...</span>
 	// 	</div>
-		
+
 	// 	</div>;
 	// }
 
-	const position: [number, number] =coords && [coords?.lat, coords?.lon];
+	const position: [number, number] = coords && [
+		coords?.lat,
+		coords?.lon,
+	];
 
 	return (
-		<div style={{ height: '300px', width: '100%' }}>
-			{weather?.isLoading ? 
-		<div role="status" className="space-y-8 w-full animate-pulse md:space-y-0 md:space-x-8 rtl:space-x-reverse ">
-				<div className="flex items-center justify-center w-full h-48 bg-gray-300 rounded sm:w-96 dark:bg-gray-300">
-					<svg className="w-10 h-10 text-gray-200 dark:text-gray-200" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 20 18">
-									<path d="M18 0H2a2 2 0 0 0-2 2v14a2 2 0 0 0 2 2h16a2 2 0 0 0 2-2V2a2 2 0 0 0-2-2Zm-5.5 4a1.5 1.5 0 1 1 0 3 1.5 1.5 0 0 1 0-3Zm4.376 10.481A1 1 0 0 1 16 15H4a1 1 0 0 1-.895-1.447l3.5-7A1 1 0 0 1 7.468 6a.965.965 0 0 1 .9.5l2.775 4.757 1.546-1.887a1 1 0 0 1 1.618.1l2.541 4a1 1 0 0 1 .028 1.011Z"/>
-					</svg>
+		<div style={{ width: '100%' }} className="md:h-full h-[300px]">
+			{weather?.isLoading ? (
+				<div
+					role="status"
+					className="space-y-8 w-full animate-pulse md:space-y-0 md:space-x-8 rtl:space-x-reverse "
+				>
+					<div className="flex items-center justify-center w-full h-48 bg-gray-300 rounded sm:w-96 dark:bg-gray-300">
+						<svg
+							className="w-10 h-10 text-gray-200 dark:text-gray-200"
+							aria-hidden="true"
+							xmlns="http://www.w3.org/2000/svg"
+							fill="currentColor"
+							viewBox="0 0 20 18"
+						>
+							<path d="M18 0H2a2 2 0 0 0-2 2v14a2 2 0 0 0 2 2h16a2 2 0 0 0 2-2V2a2 2 0 0 0-2-2Zm-5.5 4a1.5 1.5 0 1 1 0 3 1.5 1.5 0 0 1 0-3Zm4.376 10.481A1 1 0 0 1 16 15H4a1 1 0 0 1-.895-1.447l3.5-7A1 1 0 0 1 7.468 6a.965.965 0 0 1 .9.5l2.775 4.757 1.546-1.887a1 1 0 0 1 1.618.1l2.541 4a1 1 0 0 1 .028 1.011Z" />
+						</svg>
+					</div>
 				</div>
-			</div>	:
-		
-			<MapContainer
-				style={{ height: '100%', width: '100%' }}
-				center={position}
-				zoom={13}
-				scrollWheelZoom={false}
-				className="z-10"
-			>
-				<TileLayer
-					attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-					url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-				/>
-				<Marker position={position} icon={customIcon}>
-					<Popup>Location: {locationName}</Popup>
-				</Marker>
-			<MapUpdate coords={coords} />
-			</MapContainer>
-			}
+			) : (
+				<MapContainer
+					style={{ height: '100%', width: '100%' }}
+					center={position}
+					zoom={13}
+					scrollWheelZoom={false}
+					className="z-10"
+				>
+					<TileLayer
+						attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+						url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+					/>
+					<Marker position={position} icon={customIcon}>
+						<Popup>Location: {locationName}</Popup>
+					</Marker>
+					<MapUpdate coords={coords} />
+				</MapContainer>
+			)}
 		</div>
 	);
 };
